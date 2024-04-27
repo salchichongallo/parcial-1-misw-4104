88d0e650b1400c099ba7205522ea56a82f7c26af
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-04-27 13:17:27 -0500

    feat(coffee): add getCoffees method to service

--- src/app/coffee/coffees.service.spec.ts
+++ src/app/coffee/coffees.service.spec.ts
@@ -1,17 +1,36 @@
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './coffee';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [CoffeesService],
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(CoffeesService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should not get coffees', () => {
+    const EMPTY_COFFEES: Coffee[] = [];
+    service.getCoffees().subscribe(coffees => expect(coffees).toEqual([]));
+
+    const testRequest = httpController.expectOne(service.coffeesUrl);
+    testRequest.flush(EMPTY_COFFEES);
+
+    httpController.verify();
+    expect(testRequest.request.method).toEqual('GET');
   });
 });
--- src/app/coffee/coffees.service.ts
+++ src/app/coffee/coffees.service.ts
@@ -1,4 +1,16 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Coffee } from './coffee';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  public readonly coffeesUrl = environment.coffeesUrl;
+
+  constructor(private readonly http: HttpClient) {}
+
+  public getCoffees(): Observable<Coffee[]> {
+    return this.http.get<Coffee[]>(this.coffeesUrl);
+  }
+}
