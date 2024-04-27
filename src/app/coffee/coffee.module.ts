3c5908a4932198128d8f355b1d9bc8a70f04517c
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-04-27 14:16:35 -0500

    fix(coffee): import HTTP client module

--- src/app/coffee/coffee.module.ts
+++ src/app/coffee/coffee.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CoffeeComponent } from './coffee.component';
 import { CoffeesService } from './coffees.service';
 import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   declarations: [CoffeeComponent, CoffeeListComponent],
   exports: [CoffeeComponent],
   providers: [CoffeesService],
