24c0314c8398a387209b15d206c453ccc54ea5b2
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-04-27 14:18:12 -0500

    feat(coffee): render coffee table

--- src/app/coffee/coffee-list/coffee-list.component.spec.ts
+++ src/app/coffee/coffee-list/coffee-list.component.spec.ts
@@ -1,23 +1,35 @@
+import { of } from 'rxjs';
 import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { buildCoffee } from '../coffee.fixtures';
+import { CoffeesService } from '../coffees.service';
 
 import { CoffeeListComponent } from './coffee-list.component';
 
 describe('CoffeeListComponent', () => {
   let fixture: ComponentFixture<CoffeeListComponent>;
+  let coffeesService: jasmine.SpyObj<CoffeesService>;
 
   beforeEach(async () => {
+    coffeesService = jasmine.createSpyObj('CoffeesService', ['getCoffees']);
+    coffeesService.getCoffees.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
       declarations: [CoffeeListComponent],
+      providers: [
+        {
+          provide: CoffeesService,
+          useValue: coffeesService,
+        },
+      ],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(CoffeeListComponent);
-    fixture.detectChanges();
   });
 
   it('should render header table', () => {
+    fixture.detectChanges();
     const [numeral, title, type, region] = fixture.debugElement.queryAll(
       By.css('th'),
     );
@@ -25,5 +37,19 @@ describe('CoffeeListComponent', () => {
     expect(title.nativeElement.textContent).toBe('Nombre');
     expect(type.nativeElement.textContent).toBe('Tipo');
     expect(region.nativeElement.textContent).toBe('Región');
+  });
+
+  it('should render the given coffee', () => {
+    const coffee = buildCoffee();
+    coffeesService.getCoffees.and.returnValue(of([coffee]));
+    fixture.detectChanges();
+
+    const [numeral, name, type, region] = fixture.debugElement.queryAll(
+      By.css('td'),
+    );
+    expect(numeral.nativeElement.textContent).toBe('1');
+    expect(name.nativeElement.textContent).toBe(coffee.nombre);
+    expect(type.nativeElement.textContent).toBe(coffee.tipo);
+    expect(region.nativeElement.textContent).toBe(coffee.region);
   });
 });
--- src/app/coffee/coffee-list/coffee-list.component.ts
+++ src/app/coffee/coffee-list/coffee-list.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Coffee } from '../coffee';
+import { CoffeesService } from '../coffees.service';
 
 @Component({
   selector: 'app-coffee-list',
   templateUrl: './coffee-list.component.html',
   styleUrl: './coffee-list.component.css',
 })
-export class CoffeeListComponent {}
+export class CoffeeListComponent implements OnInit {
+  protected coffees: Coffee[] = [];
+
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  ngOnInit() {
+    this.getCoffees();
+  }
+
+  private getCoffees() {
+    this.coffeesService.getCoffees().subscribe(coffees => {
+      this.coffees = coffees;
+    });
+  }
+}
