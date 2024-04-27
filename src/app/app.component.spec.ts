428787d452b4ae4ddaded4d56b387301ab458c30
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-04-27 11:49:56 -0500

    build: add Bootstrap support

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('parcial-1-misw-4104');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, parcial-1-misw-4104',
-    );
-  });
 });
